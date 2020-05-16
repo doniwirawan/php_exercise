6fddf4cc0e02453643b75314960633db64ca0823
doniwirawan <doniwirawan166@gmail.com>
2020-05-16 16:18:11 +0800

    major update

--- hitung_gaji/index.php
+++ hitung_gaji/index.php
@@ -1,48 +1,89 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-    <div class="container">
-    <div class="row">
-    <div class="col-4"></div>
-    <div class="col-lg-4 xs-auto mt-lg-5 border p-4 rounded">
-        <form action="proses.php" method="post">
-        <div class="form-group">
+  <title>Hello, world!</title>
+</head>
+
+<body>
+  <?php
+
+  $nama = $gajipokok = $tunjangan = "";
+
+
+  if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $nama = test_input($_POST['nama']);
+    $gajipokok = test_input($_POST['gaji_pokok']);
+    $tunjangan = test_input($_POST['tunjangan']);
+  }
+
+  
+
+  function test_input($data)
+  {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+  $total = $gajipokok + $tunjangan;
+  $pajak = 0.05 * $total;
+  $bpjs  = 0.03 * $total;
+  $gaber = $total - $pajak - $bpjs;
+
+  ?>
+
+  <div class="container">
+    <div class="row mt-lg-5">
+      <div class="col-lg-6 xs-auto mt-lg-4 border p-4 ">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+          <div class="form-group">
             <label for="exampleInputEmail1">Nama Anda</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="nama">
-            
-        </div>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="nama" placeholder="Nama Anda">
+          </div>
 
-        <div class="form-group">
+          <div class="form-group">
             <label for="exampleInputPassword1">Gaji Pokok</label>
-            <input type="number" class="form-control" id="exampleInputPassword1" name="gaji_pokok">
-        </div>
+            <input type="number" class="form-control" id="exampleInputPassword1" name="gaji_pokok" placeholder="Gaji Pokok">
+          </div>
 
-        <div class="form-group">
+          <div class="form-group">
             <label for="exampleInputPassword1">Tunjangan</label>
-            <input type="number" class="form-control" id="exampleInputPassword1" name="tunjangan">
-        </div>    
-        <input type="submit" class="btn btn-primary" value="Submit"></input>
-        </form>
-    </div>
-    <div class="col-4"></div>
-    </div>  
-    </div>
+            <input type="number" class="form-control" id="exampleInputPassword1" name="tunjangan" placeholder="Tunjangan">
+          </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  </body>
+          <input type="submit" class="btn btn-primary" value="Submit">
+          </input>
+
+        </form>
+      </div>
+
+      <div class="col-lg-6 col-md-12 border mt-lg-4 p-4 text-capitalize ">
+
+        <h3 class="text-left">Nama anda adalah :<br><?= $nama; ?></h3>
+        <h3 class="text-left text-primary mt-lg-5">Gaji Total anda adalah : <br> Rp.<?= $total; ?> </h3>
+        <h3 class="text-left text-success mt-lg-5">Gaji Bersih anda adalah : <br> Rp.<?= $gaber; ?> </h3>
+
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
 </html>
---
+++ output_page_itself/index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <?php
+    // define variables and set to empty values
+    $name = $email = $gender = $comment = $website = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = test_input($_POST["name"]);
+        $email = test_input($_POST["email"]);
+        $website = test_input($_POST["website"]);
+        $comment = test_input($_POST["comment"]);
+        $gender = test_input($_POST["gender"]);
+    }
+
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
+
+    <h2>PHP Form Validation Example</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        Name: <input type="text" name="name">
+        <br><br>
+        E-mail: <input type="text" name="email">
+        <br><br>
+        Website: <input type="text" name="website">
+        <br><br>
+        Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+        <br><br>
+        Gender:
+        <input type="radio" name="gender" value="female">Female
+        <input type="radio" name="gender" value="male">Male
+        <input type="radio" name="gender" value="other">Other
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <?php
+    echo "<h2>Your Input:</h2>";
+    echo $name;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo $website;
+    echo "<br>";
+    echo $comment;
+    echo "<br>";
+    echo $gender;
+    ?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
